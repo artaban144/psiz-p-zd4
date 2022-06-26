fe18fab8d1df72793c3236002bc78fe833052d0c
Piotr Polok <polok.piotr144@gmail.com>
2022-06-26 18:36:11 +0200

    Added text file logger class

---
+++ FileLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psiz_p_zd4
+{
+    public class FileLogger
+    {
+        public static void LogMessage(string message)
+        {
+            File.AppendAllText("logs.txt",
+                   "[" + GetCurrentDateTimeString() + "] " + message + Environment.NewLine);
+        }
+
+        private static string GetCurrentDateTimeString()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
