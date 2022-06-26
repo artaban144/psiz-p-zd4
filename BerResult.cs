9123b19d830fc00ad7d18d1d1b015b50034ccb49
Piotr Polok <polok.piotr144@gmail.com>
2022-06-26 20:11:44 +0200

    Create DTO object for storing Ber calculation result

---
+++ BerResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psiz_p_zd4
+{
+    public class BerResult
+    {
+        public BerResult()
+        {
+            Result = 0;
+            TotalNumberOfBits = 0;
+            ErrorBits = 0;
+        }
+
+        public float Result { get; set; }
+        public double TotalNumberOfBits { get; set; }
+        public double ErrorBits { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public void StartCalculation()
+        {
+            this.StartTime = DateTime.Now;
+        }
+
+        public void StopCalculation()
+        {
+            this.EndTime = DateTime.Now;
+        }
+    }
+}
