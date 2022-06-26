b5c60e3cf0ba7e3de1b3014e38c4d5e61adf1dd9
Piotr Polok <polok.piotr144@gmail.com>
2022-06-26 21:13:42 +0200

    Add calculation time to console output and logger

--- Program.cs
+++ Program.cs
@@ -16,22 +16,42 @@ namespace psiz_p_zd4
             string inputFileTwo;
             inputFileTwo = Console.ReadLine();
 
+            FileLogger.LogMessage("Starting calculation");
+
             try
             {
                 BerResult berResult = (new BerCalculator(inputFileOne, inputFileTwo)).CalculateBer();
                 PrintResult(berResult);
+                LogResult(berResult);
             } catch (Exception exception)
             {
                 Console.WriteLine("Error occured. Check input files.");
                 return;
             }
 
+            FileLogger.LogMessage("Calculation closed");
         }
         static void PrintResult(BerResult berResult)
         {
             Console.WriteLine("Ber: " + berResult.Result);
             Console.WriteLine("Total number of bits: " + berResult.TotalNumberOfBits);
             Console.WriteLine("Error bits: " + berResult.ErrorBits);
+            Console.WriteLine("Time elapsed: " + GetCalculationTimeInMiliseconds(berResult) + "ms");
+        }
+
+        static void LogResult(BerResult berResult)
+        {
+            FileLogger.LogMessage("Ber: " + berResult.Result);
+            FileLogger.LogMessage("Total number of bits: " + berResult.TotalNumberOfBits);
+            FileLogger.LogMessage("Error bits: " + berResult.ErrorBits);
+            FileLogger.LogMessage("Time elapsed: " + GetCalculationTimeInMiliseconds(berResult) + "ms");
+        }
+
+        static string GetCalculationTimeInMiliseconds(BerResult berResult)
+        {
+            TimeSpan elapsed = berResult.EndTime - berResult.StartTime;
+
+            return elapsed.TotalMilliseconds.ToString();
         }
     }
 }
